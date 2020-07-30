e7a2b9e842b34ab63cd177543d0a603e86b39c92
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2020-07-30 13:57:20 +0000

    adjustments for patients, treatments and diagnostics.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os 
+from datetime import datetime
 from flask import Flask, request, jsonify, url_for 
 from flask import Flask, jsonify, request
 from flask_jwt_simple import JWTManager, jwt_required, create_jwt, get_jwt_identity
@@ -10,7 +11,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Medicalstaff, Treatment, Patient, Diagnostic, Login
+from models import db, User, Medicalstaff, Treatment, Patient, Diagnostic
 #from models import Person 
 
 app = Flask(__name__)
@@ -81,21 +82,22 @@ def login():
     if not request.is_json:
         return jsonify({"msg": "Missing JSON in request"}), 400
     params = request.get_json()
-    username = params.get('username', None)
+    email = params.get('email', None)
     password = params.get('password', None)
     
-    if not username:
+    if not email:
         return jsonify({"msg": "Missing username parameter"}), 400
     if not password:
         return jsonify({"msg": "Missing password parameter"}), 400
-    usercheck = Medicalstaff.query.filter_by(username=username, password=password).first()
-    if usercheck == None:
+    user = Medicalstaff.query.filter_by(email=email).one_or_none()
+    
+    if user is None:
+        return jsonify({"msg": "not found"}), 404
+
+    if user.password != password:
         return jsonify({"msg": "Bad username or password"}), 401
 
-    # if username != 'test' or password != 'test':
-    #     return jsonify({"msg": "Bad username or password"}), 401
-    # Identity can be any data that is json serializable
-    ret = {'jwt': create_jwt(identity=username)}
+    ret = {'jwt': create_jwt(identity=user.id)}
     return jsonify(ret), 200
 
 @app.route('/medicalstaff', methods=['POST', 'GET'])
@@ -132,6 +134,7 @@ def handle_medicalstaff():
 
 
 @app.route('/treatment', methods=['POST', 'GET'])
+@jwt_required
 def handle_treatment():
     """
     Create person and retrieve all users
@@ -144,14 +147,13 @@ def handle_treatment():
         if 'name' not in body:
             raise APIException('You need to specify the name', status_code=400)
         user1 = Treatment(
-            doctor_id=body['doctor_id'],
+            doctor_id=get_jwt_identity(),
             patient_id=body['patient_id'],
-            diagnostic_id=body['diagnostic_id'],
             name=body['name'],
-            date=['date'],
+            date=datetime.strptime(body['date'], "%m-%d-%Y"),
             hospital=body['hospital'],
             room=body['room'],
-            covidtestresult=body['covidtestresult'],
+            covidtest=body['covidtestresult'],
             status=body['status'],
             notes=body['notes'],
             )
@@ -166,42 +168,60 @@ def handle_treatment():
     return "Invalid Method", 404
 
 @app.route('/patient', methods=['POST', 'GET'])
+@jwt_required
 def handle_patient():
     """
     Create person and retrieve all users
     """
     # POST request
+
     if request.method == 'POST':
         body = request.get_json()
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
-        if 'first_name' not in body:
-            raise APIException('You need to specify the first name', status_code=400)
-        if 'last_name' not in body:
-            raise APIException('You need to specify the last name', status_code=400)       
+        if (
+            'first_name' not in body or
+            'last_name' not in body or
+            'date' not in body or
+            'birth_date' not in body or
+            'gender' not in body or
+            'address' not in body or
+            'email' not in body or
+            'phone_number' not in body or 
+            'id_number' not in body
+        ):
+            raise APIException('Please check your input', status_code=400)
+        print(body['date'])
         user1 = Patient(
-            date=body['date'],
+            date= datetime.strptime(body['date'], "%m-%d-%Y"),
             first_name=body['first_name'],
             last_name=body['last_name'],
-            birth_date =body['birth_date'],
-            gender =['gender'],
-            address =['address'],
-            email = ['email'],
-            phone_number =['phone_number'],
-            id_number =['id_number']
-            )
-        db.session.add(user1)
-        db.session.commit()
-        return "ok", 200
+            birth_date = datetime.strptime(body['birth_date'], "%m-%d-%Y"),
+            gender = body['gender'],
+            address = body['address'],
+            email = body['email'],
+            phone_number = body['phone_number'],
+            id_number = body['id_number'],
+            doctor_id = get_jwt_identity()
+        )
+        try: 
+            db.session.add(user1)
+            db.session.commit()
+            return jsonify(user1.serialize()), 201
+        except Exception as error:
+            db.session.rollback()
+            print(error)
+            return "bad request", 400
     # GET request
     if request.method == 'GET':
-        all_people = Patient.query.all()
+        all_people = Patient.query.filter_by(doctor_id=get_jwt_identity()).all()
         all_people = list(map(lambda x: x.serialize(), all_people))
         return jsonify(all_people), 200
     return "Invalid Method", 404
 
 
 @app.route('/diagnostic', methods=['POST', 'GET'])
+@jwt_required
 def handle_diagnostic():
     """
     Create person and retrieve all users
@@ -216,14 +236,15 @@ def handle_diagnostic():
         user1 = Diagnostic(
             patient_id=body['patient_id'],
             name=body['name'],
-            datetime=body['datetime'],
+            date_time=datetime.strptime(body['date_time'], "%m-%d-%Y"),
             status=body['status'],
-            covidtest=body['covidtest'],
+            covidtestresult=body['covidtestresult'],
             symptoms =body['symptoms'],
             admission =body['admission'],
             notes=body['notes'],
             prescription=body['prescription'],
-            totalcost=body['totalcost'],
+            total_cost=body['total_cost'],
+            treatment_id=body["treatment_id"]
             )
         db.session.add(user1)
         db.session.commit()
